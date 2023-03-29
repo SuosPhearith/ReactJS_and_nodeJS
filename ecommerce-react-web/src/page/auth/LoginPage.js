efabc5201e42cfd858f1a441572d298f77fc56f1
SuosPhearith <suosphearith@gmail.com>
2023-03-29 23:31:12 +0700

    LoginForm

--- ecommerce-react-web/src/page/auth/LoginPage.js
+++ ecommerce-react-web/src/page/auth/LoginPage.js
@@ -1,45 +1,82 @@
 import React, { useState } from 'react'
 import { request } from '../../util/api'
-import { message } from 'antd'
+import { Button, Form, message,Input,Checkbox } from 'antd'
+import "./LoginPage.css"
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 
 export default function LoginPage() {
 
-  const [username,setUsername] = useState("")
-  const [password,setPassword] = useState("")
+  const onFinish = (fields) => {
 
-  const handleLogin = () => {
     var params = {
-      "username" : username,
-      "password" : password
+      "username": fields.username,
+      "password": fields.password
     }
-    request("post","customer/login",params).then(res=>{
-       if(res.data && res.data.is_login){
-          // message.success("Login success")
-          localStorage.setItem("is_login","1") // is_login = 1
-          localStorage.setItem("profile",JSON.stringify(res.data.profile))
-          window.location.href = "/"
-       }else{
-          message.warning(res.data.message)
-       }
+    request("post", "customer/login", params).then(res => {
+      if (res.data && res.data.is_login) {
+        // message.success("Login success")
+        localStorage.setItem("is_login", "1") // is_login = 1
+        localStorage.setItem("profile", JSON.stringify(res.data.profile))
+        window.location.href = "/"
+      } else {
+        message.warning(res.data.message)
+      }
     })
   }
 
   return (
-    <div>
-       <h1>Login Page</h1>
-        <input
-          placeholder='username'
-          onChange={(event)=>{
-            setUsername(event.target.value)
+    <>
+      <div className='loging-form'>
+        <h1>Login</h1>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
           }}
-        />
-        <input 
-          placeholder='password'
-          onChange={(event)=>{
-            setPassword(event.target.value)
-          }}
-        />
-        <button onClick={handleLogin}>Login</button>
-    </div>
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <div className="login-form-forgot">
+            <a href="">Forgot password</a>
+          </div>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            <div>
+              Or <a href="">register now!</a>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   )
 }
